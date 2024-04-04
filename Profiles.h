a68f611388dfced327ec245983b264a27b2460cd
Avisheksap <144167655+Avisheksap@users.noreply.github.com>
2024-04-04 19:46:38 -0400

    Updated Profiles.h

--- Profiles.h
+++ Profiles.h
@@ -5,7 +5,17 @@
 #ifndef PROFILES_H
 #define PROFILES_H
 
+#include <stdbool.h> // Include for boolean type
+
 #define MAX_PROFILES 10
+#define MAX_GAMES 50
+
+// Struct for a game in the catalog
+typedef struct {
+    char title[100];
+    int year;
+    float rating;
+} Game;
 
 // Struct for a user profile
 typedef struct {
@@ -13,7 +23,10 @@ typedef struct {
     char firstName[50];
     char lastName[50];
     char gamertag[50];
+    char password[50]; // Password field
     int isActive; // Flag to indicate if the profile is active or deleted
+    Game gameCatalog[MAX_GAMES]; // Array to store the game catalog
+    int totalGames; // Number of games in the catalog
 } UserProfile;
 
 extern UserProfile profiles[MAX_PROFILES];
@@ -25,6 +38,6 @@ void create_profile();
 void update_profile();
 void delete_profile();
 void display_profiles();
+int verify_password(int userID, const char* password);
 
 #endif /* PROFILES_H */
-
